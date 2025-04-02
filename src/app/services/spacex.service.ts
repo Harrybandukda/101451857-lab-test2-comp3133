efa3ccfea61eb642813c53ff8ab050f1a3458593
harrybandukda <harrisbandukda@gmail.com>
2025-04-02 22:14:39 +0500

    updated services

--- src/app/services/spacex.service.ts
+++ src/app/services/spacex.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mission } from '../models/mission.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpacexService {
+  private apiUrl = 'https://api.spacexdata.com/v3/launches';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllMissions(): Observable<Mission[]> {
+    return this.http.get<Mission[]>(this.apiUrl);
+  }
+
+  getMissionByFlightNumber(flightNumber: number): Observable<Mission> {
+    return this.http.get<Mission>(`${this.apiUrl}/${flightNumber}`);
+  }
+
+  getMissionsByYear(year: string): Observable<Mission[]> {
+    return this.http.get<Mission[]>(`${this.apiUrl}?launch_year=${year}`);
+  }
 }
