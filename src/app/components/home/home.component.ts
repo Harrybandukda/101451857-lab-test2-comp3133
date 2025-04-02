ad3adbce24e57cd70e7f662c270a2fef15f0ef85
harrybandukda <harrisbandukda@gmail.com>
2025-04-03 01:27:26 +0500

    implemented home screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatCardModule, RouterLink]
 })
 export class HomeComponent {
-
 }
