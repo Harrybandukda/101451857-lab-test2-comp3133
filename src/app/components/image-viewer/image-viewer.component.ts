8450f87a8c301dd7351cc645a7e794447b23c124
harrybandukda <harrisbandukda@gmail.com>
2025-04-03 02:34:03 +0500

    implemented image viewer from images from the api

--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+interface DialogData {
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-image-viewer',
-  imports: [],
   templateUrl: './image-viewer.component.html',
-  styleUrl: './image-viewer.component.scss'
+  styleUrls: ['./image-viewer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule]
 })
 export class ImageViewerComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ImageViewerComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
